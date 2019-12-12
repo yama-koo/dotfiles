31daf373bb9655359e90f911906271257410d57a
yama-koo <yamako.ilc@gmail.com>
2019-12-13 01:07:58 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+rm -rf "$HOME/Library/Application Support/Code/User/settings.json"
 ln $HOME/dotfiles/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
